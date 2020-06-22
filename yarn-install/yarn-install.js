802e43dea139c2d20b7a467d592c6157b5bc2e89
Cameron Knight <cameron.knight@bumped.com>
2020-06-22 13:46:19 -0700

    ⬆️ upgrade dependencies, rebuild
    - this is primarily to solve security issues with @actions/core
    - intended to replace https://github.com/bumped-inc/github-actions/pull/2

--- yarn-install/yarn-install.js
+++ yarn-install/yarn-install.js
@@ -1,22 +1,34 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
 var __importStar = (this && this.__importStar) || function (mod) {
     if (mod && mod.__esModule) return mod;
     var result = {};
-    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
-    result["default"] = mod;
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
     return result;
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const core = __importStar(require("@actions/core"));
 const exec_1 = require("../shared/exec");
 async function run() {
-    const npmToken = core.getInput('npm-token');
-    await exec_1.exec('yarn', ['--non-interactive', 'install'], {
+    const npmToken = core.getInput("npm-token");
+    await exec_1.exec("yarn", ["--non-interactive", "install"], {
         env: {
             NODE_AUTH_TOKEN: npmToken,
         },
     });
-    await exec_1.exec('git', ['diff', '--exit-code', 'yarn.lock']);
+    await exec_1.exec("git", ["diff", "--exit-code", "yarn.lock"]);
 }
 run().catch((error) => {
     core.setFailed(error.message);
--- yarn-install/yarn-install.ts
+++ yarn-install/yarn-install.ts
@@ -5,12 +5,12 @@ async function run(): Promise<void> {
   const npmToken = core.getInput("npm-token");
   await exec("yarn", ["--non-interactive", "install"], {
     env: {
-      NODE_AUTH_TOKEN: npmToken
-    }
+      NODE_AUTH_TOKEN: npmToken,
+    },
   });
   await exec("git", ["diff", "--exit-code", "yarn.lock"]);
 }
 
-run().catch(error => {
+run().catch((error) => {
   core.setFailed(error.message);
 });
